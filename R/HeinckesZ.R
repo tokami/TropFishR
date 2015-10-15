7f9ef44a7754368fe37e6f2eb10e8c87fc2342b0
tokami <joslas@web.de>
2015-10-15 13:09:26 +0200

    HeinckeZ finished

--- R/HeinckesZ.R
+++ R/HeinckesZ.R
@@ -8,6 +8,9 @@
 #'
 #' @examples
 #' data("ex.HeinckesZ")
+#' HeinckesZ(cohort <- ex.HeinckesZ$cohort,
+#'  age <- ex.HeinckesZ$age.t1,
+#'  CPUE <- ex.HeinckesZ$CPUE)
 #'
 #' @details Z from CPUE data
 #'
@@ -17,37 +20,26 @@
 #' @export
 
 
-data("ex.HeinckesZ")
-
-#HeinckesZ(
-  cohort <- ex.CPUE_Z$cohort
-  age <- ex.CPUE_Z$age.t1
-  CPUE <- ex.CPUE_Z$CPUE
 
 HeinckesZ <- function(cohort, age, CPUE){
 
-  df.HZ <- data.frame(cohort = cohort,
-                      age.t1 = age,
-                      CPUE = CPUE)
+  df.HZ <- data.frame(cohort = cohort[1:(length(cohort)-1)],
+                      age.t1 = age[1:(length(age)-1)],
+                      CPUE = CPUE[1:(length(CPUE)-1)])
 
   result_Z <- list()
   for(i in 2:length(age)){
-    i=2
-    for(k in 1:i){
-      Zi <- round((1 / (age[i] - age[i-1])) *
-                    (log(CPUE[i-1] /CPUE[i])),digits = 2)
+    Zi <- rep(NA,(length(age)-1))
+    for(k in 1:(i-1)){
+      Zi[k] <- round((1 / (age[i] - age[k])) *
+                    (log(CPUE[k] /CPUE[i])),digits = 2)
     }
+    result_Z[[i-1]] <- Zi
   }
 
-
-  for(i in 1:(length(CPUE)-1)){
-    Zi <- round((1 / (age[i+1] - age[i])) * (log(CPUE[i] /CPUE[i+1])),digits = 2)
-    delta_ti <- paste("t",i+1,"-t",i,sep='')
-    result_Z[[i]] <- c(delta_ti,Zi)
+  for(i in 1:length(result_Z)){
+    df.HZ[[paste("Z",cohort[i+1],sep='_')]] <- result_Z[[i]]
   }
 
-  result_Z <- do.call(rbind,result_Z)
-  names(result_Z) <- c("time interval","Z")
-  print(result_Z)
-
+  print(df.HZ)
 }
