70dcea779473eeefa402e87dd53cf9bd68e6c025
tokami <joslas@web.de>
2015-11-06 16:33:00 +0100

    first prediction model

--- R/Predict_BevertonHolt.R
+++ R/Predict_BevertonHolt.R
@@ -11,8 +11,17 @@
 #' @param Winf Infinite weight in grams!
 #'
 #' @examples
-#'
-#'
+#' #age data example 1
+#' # Nemipterus marginatus
+#' Predict_BevertonHolt(K = 0.37,M = 1.1,T_Lc = seq(0.2,1,0.2),T_Lr = 0.4,t0 = -0.2,W_Linf = 286,FM = seq(0,6,0.1),datatype = 'age')
+#' #where it is maximal  = MSY
+#' #age data example 2
+#' #Leiognathus spendens (Pauly 1980)
+#' Predict_BevertonHolt(W_Linf = 64,K = 1,t0 = -0.2,T_Lr = 0.2,M = 1.8,T_Lc = c(0.2,0.3,1.0),datatype = 'age')
+#' #length data example
+#' #Xiphias gladius (Berkeley and Houde 1980)
+#' Predict_BevertonHolt(W_Linf = 309,K = 0.0949,M = 0.18,T_Lc = c(100,118,150,180),T_Lr = 90 ,datatype = 'length',a=0.0003,b=3) ## T_Lr , a, b ??? assumed
+#' ####test: E <- seq(0,0.9,0.1) F_PBH <- E * M / (1 -E)
 #' @details For variable parameter system vectors are reuqired for constant parameter systems matrices or data.frames have to be inserted. or vectors The length converted linearised catch curve is used to calculate the total mortality (Z). This function includes a so called locator function, which asks you to choose points from a graph manually. Based on these points the regression line is calculated.
 #'
 #' @references
@@ -20,53 +29,8 @@
 #'
 #' @export
 
-
-#age data example 1
-# Nemipterus marginatus
-K = 0.37
-M = 1.1
-Tc = c(0.2,0.3,1.0)
-Tr = 0.4
-t0 = -0.2
-Winf = 286
-FM = seq(0,6,0.1)
-
-Predict_BevertonHolt(K = 0.37,
-                     M = 1.1,
-                     Tc = 1.0,
-                     Tr = 0.4,
-                     t0 = -0.2,
-                     Winf = 286,
-                     FM = seq(0,6,0.1))
-#where it is maximal  = MSY
-
-############
-
-#age data example 2
-#Leiognathus spendens (Pauly 1980)
-Winf = 64
-K = 1
-t0 = -0.2
-Tr = 0.2
-M = 1.8
-Tc = c(0.2,0.3,1.0)
-
-############
-
-#length data example
-#Xiphias gladius (Berkeley and Houde 1980)
-Linf = 309
-K = 0.0949
-M = 0.18
-Lc = c(100,118,150,180) #Lc = c(118,150)
-Lr = 90 #??? assumed
-
-
-Predict_BevertonHolt <- function(Winf = NA, Linf = NA, K, t0 = NA,
-                                  M, Tr = NA, Tc = NA, Lc = NA, FM = NA){
-
-  if(is.na(Winf) & is.na(Linf)) stop("You have to provide Linf or Winf!")
-
+Predict_BevertonHolt <- function(W_Linf, K, M, T_Lr, T_Lc, t0 = NA,
+                                  FM = NA, datatype,a=NA,b=NA){
 
   F_PBH <- FM
   if(length(F_PBH) == 1 & is.na(F_PBH[1])){
@@ -75,160 +39,190 @@ Predict_BevertonHolt <- function(Winf = NA, Linf = NA, K, t0 = NA,
     }
 
 
-  #with age data
-  list_Tc_runs <- list()
-  for(i in 1:length(Tc)){
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  #                        Age data                          #
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  if(datatype == 'age'){
+    Winf <- W_Linf
+    Tr <- T_Lr
+    Tc <- T_Lc
+    list_Tc_runs <- list()
+    for(i in 1:length(Tc)){
+      Tci <- Tc[i]
 
-    Tci <- Tc[i]
+      #Biomass per Recruit
+      S.PBH <- exp(-K * (Tci - t0))
+      B_R.PBH <- exp(-M*(Tci-Tr)) * Winf *
+        ((1/(F_PBH+M)) - ((3*S.PBH)/((M+F_PBH)+K)) +
+           ((3*(S.PBH^2))/((M+F_PBH)+(2*K))) - ((S.PBH^3)/((M+F_PBH) + (3*K))))
 
-    #Biomass per Recruit
-    S.PBH <- exp(-K * (Tci - t0))
-    B_R.PBH <- exp(-M*(Tci-Tr)) * Winf *
-      ((1/(F_PBH+M)) - ((3*S.PBH)/((M+F_PBH)+K)) +
-         ((3*(S.PBH^2))/((M+F_PBH)+(2*K))) - ((S.PBH^3)/((M+F_PBH) + (3*K))))
+      #virgin biomass
+      Bv_R.PBH <- B_R.PBH[which(F_PBH == 0)]
+      #biomass of exploited part of the cohort (biomass of fish older than Tc)
 
-    #virgin biomass
-    Bv_R.PBH <- B_R.PBH[which(F_PBH == 0)]
-    #biomass of exploited part of the cohort (biomass of fish older than Tc)
+      #biomass in percetage of virgin biomass
+      B_R.PBH.percent <- round((B_R.PBH / Bv_R.PBH ) * 100, digits = 1)
 
-    #biomass in percetage of virgin biomass
-    B_R.PBH.percent <- round((B_R.PBH / Bv_R.PBH ) * 100, digits = 1)
+      #Yield per Recruit
+      Y_R.PBH <- B_R.PBH * F_PBH
 
-    #Yield per Recruit
-    Y_R.PBH <- B_R.PBH * F_PBH
-
-    #mean age in annual yield
-    Ty <- (1 / (M+F_PBH)) + Tci
-
-    #mean length in the annual yield
-    Ly <- Linf * (1 - (((M+F_PBH)*S.PBH)/((M+F_PBH)+K)))
-
-    #mean weight in annual yield
-    Wy <- (M+F_PBH) * Winf *
-      ((1/(F_PBH+M)) - ((3*S.PBH)/((M+F_PBH)+K)) +
-         ((3*(S.PBH^2))/((M+F_PBH)+(2*K))) - ((S.PBH^3)/((M+F_PBH) + (3*K))))
+      #relative yield per recruit - mostly done with length frequency data (exclusively?)
+      Y_R.PBH.rel <- Y_R.PBH * (exp(-M *(Tr - t0))) / Winf
 
 
-    results.PBH <- data.frame(FM = F_PBH,
-                              Y_R = Y_R.PBH,
-                              B_R = B_R.PBH,
-                              B_R.percent = B_R.PBH.percent,
-                              Ty = Ty,
-                              Ly = Ly,
-                              Wy = Wy)
+      #mean age in annual yield
+      Ty <- (1 / (M+F_PBH)) + Tci
+
+      #mean length in the annual yield
+      #Ly <- Linf * (1 - (((M+F_PBH)*S.PBH)/((M+F_PBH)+K)))
+
+      #mean weight in annual yield
+      Wy <- (M+F_PBH) * Winf *
+        ((1/(F_PBH+M)) - ((3*S.PBH)/((M+F_PBH)+K)) +
+           ((3*(S.PBH^2))/((M+F_PBH)+(2*K))) - ((S.PBH^3)/((M+F_PBH) + (3*K))))
 
 
-    list_Tc_runs[[i]] <- results.PBH
+      results.PBH <- data.frame(FM = F_PBH,
+                                Y_R = Y_R.PBH,
+                                Y_R.rel = Y_R.PBH.rel,
+                                B_R = B_R.PBH,
+                                B_R.percent = B_R.PBH.percent,
+                                Ty = Ty,
+                                Wy = Wy)
 
-  }
-  names(list_Tc_runs) <- Tc
 
-  #plot
-  Tc_start <- which(Tc == max(Tc,na.rm=T))
-  offset_text <- list_Tc_runs[[Tc_start]]$Y_R[length(list_Tc_runs[[Tc_start]]$Y_R)] *
-    0.02
-  par(mar = c(5, 4, 4, 4) + 0.3)
-  plot(list_Tc_runs[[Tc_start]]$FM, list_Tc_runs[[Tc_start]]$Y_R, type = 'l',
-       ylab = "Y/R", xlab = "F",lty=Tc_start)
-  text(x = list_Tc_runs[[Tc_start]]$FM[length(list_Tc_runs[[Tc_start]]$FM)],
-       y = (list_Tc_runs[[Tc_start]]$Y_R[length(list_Tc_runs[[Tc_start]]$Y_R)] +
-              offset_text),
-       labels = bquote(Tc[.(names(list_Tc_runs)[Tc_start])]))
-  seq_Tc <- 1:length(list_Tc_runs)
-  seq_Tc <- seq_Tc[-Tc_start]
-  for(j in seq_Tc){
-    lines(list_Tc_runs[[j]]$FM, list_Tc_runs[[j]]$Y_R, type = 'l',
-          ylab = "Y/R", xlab = "F",lty = j)
-    text(x = list_Tc_runs[[j]]$FM[length(list_Tc_runs[[j]]$FM)],
-         y = (list_Tc_runs[[j]]$Y_R[length(list_Tc_runs[[j]]$Y_R)] +
+      list_Tc_runs[[i]] <- results.PBH
+
+    }
+    names(list_Tc_runs) <- Tc
+
+    #plot Y/R & B/R
+    Tc_start <- which(Tc == max(Tc,na.rm=T))
+    offset_text <- list_Tc_runs[[Tc_start]]$Y_R[length(list_Tc_runs[[Tc_start]]$Y_R)] *
+      0.02
+    par(mar = c(5, 4, 4, 4) + 0.3)
+    plot(list_Tc_runs[[Tc_start]]$FM, list_Tc_runs[[Tc_start]]$Y_R, type = 'l',
+         ylab = "Y/R", xlab = "F",lty=Tc_start)
+    text(x = list_Tc_runs[[Tc_start]]$FM[length(list_Tc_runs[[Tc_start]]$FM)],
+         y = (list_Tc_runs[[Tc_start]]$Y_R[length(list_Tc_runs[[Tc_start]]$Y_R)] +
                 offset_text),
-         labels = bquote(Tc[.(names(list_Tc_runs)[j])]))
-  } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
-  par(new=T)
-  plot(list_Tc_runs[[1]]$FM, list_Tc_runs[[1]]$B_R, type = 'l',
-       axes = F, ylab = '', xlab ='', lty = Tc_start,
-       col = 'blue')
-  axis(side = 4, at = pretty(range(list_Tc_runs[[1]]$B_R)), col = 'blue',
-       col.axis = 'blue')
-  mtext(side = 4, text = "B/R", line = 3, col = 'blue')
-  for(j in seq_Tc){
-    lines(list_Tc_runs[[j]]$FM, list_Tc_runs[[j]]$B_R, type = 'l',
-          ylab = "Y/R", xlab = "F", col='blue', lty = j)
-  } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
-
-
-  #with length data
-  list_Lc_runs <- list()
-  for(i in 1:length(Lc)){
-
-    Lci <- Lc[i]
-
-    #test:
-    E <- seq(0,0.9,0.1)
-    F_PBH <- E * M / (1 -E)
-
-    #E <- F_PBH/(F_PBH + M)
-
-
-    #biomass per recruit for length data?
-
-    #yiel per recurit for length data   # also possbile inputing option: F/K
-    S.PBH <- 1 - (Lci/Linf)  # == U  ##(U <- 1 - (Lci/Linf))
-    A.PBH <- ((Linf - Lci)/(Linf-Lr))^(M/K)
-    Y_R.PBH <- F_PBH * A.PBH * Winf * ( (1/(M+F_PBH)) - (3*S.PBH)/((M+F_PBH)+K) +
-                                          (3*S.PBH^2)/((M+F_PBH)+2*K) -
-                                          (S.PBH^3)/((M+F_PBH)+3*K))
-
-    #relative yield per recruit - mostly done with length frequency data (exclusively?)
-    m <- K/(M+F_PBH)
-    Y_R.PBH.rel <- E * S.PBH^(M/K) * (1 - ((3*S.PBH)/(1+m)) +
-                                    ((3*S.PBH^2)/(1+2*m)) - ((S.PBH^3)/(1+3*m)))
-
-    #mean length in the annual yield
-    Ly <- Linf * (1 - (((M+F_PBH)*S.PBH)/((M+F_PBH)+K)))
-
-    #mean weight in annual yield
-    Wy <- (M+F_PBH) * Winf *
-      ((1/(F_PBH+M)) - ((3*S.PBH)/((M+F_PBH)+K)) +
-         ((3*(S.PBH^2))/((M+F_PBH)+(2*K))) - ((S.PBH^3)/((M+F_PBH) + (3*K))))
-
-
-    results.PBH <- data.frame(FM = F_PBH,
-                              Ly = Ly,
-                              Wy = Wy,
-                              E = E,
-                              Y_R.rel = Y_R.PBH.rel)
-
-
-    list_Lc_runs[[i]] <- results.PBH
-
+         labels = bquote(Tc[.(names(list_Tc_runs)[Tc_start])]))
+    seq_Tc <- 1:length(list_Tc_runs)
+    seq_Tc <- seq_Tc[-Tc_start]
+    for(j in seq_Tc){
+      lines(list_Tc_runs[[j]]$FM, list_Tc_runs[[j]]$Y_R, type = 'l',
+            ylab = "Y/R", xlab = "F",lty = j)
+      text(x = list_Tc_runs[[j]]$FM[length(list_Tc_runs[[j]]$FM)],
+           y = (list_Tc_runs[[j]]$Y_R[length(list_Tc_runs[[j]]$Y_R)] +
+                  offset_text),
+           labels = bquote(Tc[.(names(list_Tc_runs)[j])]))
+    } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
+    par(new=T)
+    plot(list_Tc_runs[[1]]$FM, list_Tc_runs[[1]]$B_R, type = 'l',
+         axes = F, ylab = '', xlab ='', lty = Tc_start,
+         col = 'blue')
+    axis(side = 4, at = pretty(range(list_Tc_runs[[1]]$B_R)), col = 'blue',
+         col.axis = 'blue')
+    mtext(side = 4, text = "B/R", line = 3, col = 'blue')
+    for(j in seq_Tc){
+      lines(list_Tc_runs[[j]]$FM, list_Tc_runs[[j]]$B_R, type = 'l',
+            ylab = "Y/R", xlab = "F", col='blue', lty = j)
+    } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
   }
-  names(list_Lc_runs) <- Lc
 
 
-  #plot
-  Lc_start <- which(Lc == max(Lc,na.rm=T))
-  offset_text <- list_Lc_runs[[Lc_start]]$Y_R.rel[length(list_Lc_runs[[Lc_start]]$Y_R.rel)] *
-    0.02
-  par(mar = c(5, 4, 4, 4) + 0.3)
-  plot(list_Lc_runs[[Lc_start]]$FM, list_Lc_runs[[Lc_start]]$Y_R, type = 'l',
-       ylab = "rel. Y/R", xlab = "F",lty=Lc_start)
-  text(x = list_Lc_runs[[Lc_start]]$FM[length(list_Lc_runs[[Lc_start]]$FM)],
-       y = (list_Lc_runs[[Lc_start]]$Y_R[length(list_Lc_runs[[Lc_start]]$Y_R)] +
-              offset_text),
-       labels = bquote(Lc[.(names(list_Lc_runs)[Lc_start])]))
-  seq_Lc <- 1:length(list_Lc_runs)
-  seq_Lc <- seq_Lc[-Lc_start]
-  for(j in seq_Lc){
-    lines(list_Lc_runs[[j]]$FM, list_Lc_runs[[j]]$Y_R, type = 'l',lty = j)
-    text(x = list_Lc_runs[[j]]$FM[length(list_Lc_runs[[j]]$FM)],
-         y = (list_Lc_runs[[j]]$Y_R[length(list_Lc_runs[[j]]$Y_R)] +
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  #                       Length data                        #
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  if(datatype == 'length'){
+    Linf <- W_Linf
+    Lr <- T_Lr
+    Lc <- T_Lc
+    list_Lc_runs <- list()
+    for(i in 1:length(Lc)){
+
+      Lci <- Lc[i]
+
+      E <- F_PBH/(F_PBH + M)
+      # transform Linf in Winf #### CHECK!!
+      Winf <- a * (Linf ^ b)
+
+      #yiel per recurit for length data   # also possbile inputing option: F/K
+      S.PBH <- 1 - (Lci/Linf)  # == U  ##(U <- 1 - (Lci/Linf))
+      A.PBH <- ((Linf - Lci)/(Linf-Lr))^(M/K)
+      Y_R.PBH <- F_PBH * A.PBH * Winf * ((1/(M+F_PBH)) - (3*S.PBH)/((M+F_PBH)+K) +
+                                            (3*S.PBH^2)/((M+F_PBH)+2*K) -
+                                            (S.PBH^3)/((M+F_PBH)+3*K))
+
+      #biomass per recruit for length data?
+      B_R.PBH <- Y_R.PBH / F_PBH
+
+      #virgin biomass
+      Bv_R.PBH <- B_R.PBH[which(F_PBH == 0)]
+      #biomass of exploited part of the cohort (biomass of fish older than Tc)
+
+      #biomass in percetage of virgin biomass
+      B_R.PBH.percent <- round((B_R.PBH / Bv_R.PBH ) * 100, digits = 1)
+
+      #relative yield per recruit - mostly done with length frequency data (exclusively?)
+      m <- K/(M+F_PBH)
+      Y_R.PBH.rel <- E * S.PBH^(M/K) * (1 - ((3*S.PBH)/(1+m)) +
+                                          ((3*S.PBH^2)/(1+2*m)) - ((S.PBH^3)/(1+3*m)))
+
+      #mean length in the annual yield
+      Ly <- Linf * (1 - (((M+F_PBH)*S.PBH)/((M+F_PBH)+K)))
+
+      #mean weight in annual yield
+     # Wy <- (M+F_PBH) * Winf *
+     #    ((1/(F_PBH+M)) - ((3*S.PBH)/((M+F_PBH)+K)) +
+     #       ((3*(S.PBH^2))/((M+F_PBH)+(2*K))) - ((S.PBH^3)/((M+F_PBH) + (3*K))))
+
+
+      results.PBH <- data.frame(FM = F_PBH,
+                                Ly = Ly,
+                                E = E,
+                                Y_R = Y_R.PBH,
+                                Y_R.rel = Y_R.PBH.rel,
+                                B_R = B_R.PBH,
+                                B_R.pecent = B_R.PBH.percent)
+
+
+      list_Lc_runs[[i]] <- results.PBH
+
+    }
+    names(list_Lc_runs) <- Lc
+
+    #plot Y/R & B/R
+    Lc_start <- which(Lc == max(Lc,na.rm=T))
+    offset_text <- list_Lc_runs[[Lc_start]]$Y_R[length(list_Lc_runs[[Lc_start]]$Y_R)] *
+      0.02
+    par(mar = c(5, 4, 4, 4) + 0.3)
+    plot(list_Lc_runs[[Lc_start]]$FM, list_Lc_runs[[Lc_start]]$Y_R, type = 'l',
+         ylab = "Y/R", xlab = "F",lty=Lc_start)
+    text(x = list_Lc_runs[[Lc_start]]$FM[length(list_Lc_runs[[Lc_start]]$FM)],
+         y = (list_Lc_runs[[Lc_start]]$Y_R[length(list_Lc_runs[[Lc_start]]$Y_R)] +
                 offset_text),
-         labels = bquote(Lc[.(names(list_Lc_runs)[j])]))
-  } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
-
+         labels = bquote(Lc[.(names(list_Lc_runs)[Lc_start])]))
+    seq_Lc <- 1:length(list_Lc_runs)
+    seq_Lc <- seq_Lc[-Lc_start]
+    for(j in seq_Lc){
+      lines(list_Lc_runs[[j]]$FM, list_Lc_runs[[j]]$Y_R, type = 'l',
+            ylab = "Y/R", xlab = "F",lty = j)
+      text(x = list_Lc_runs[[j]]$FM[length(list_Lc_runs[[j]]$FM)],
+           y = (list_Lc_runs[[j]]$Y_R[length(list_Lc_runs[[j]]$Y_R)] +
+                  offset_text),
+           labels = bquote(Lc[.(names(list_Lc_runs)[j])]))
+    } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
+    par(new=T)
+    plot(list_Lc_runs[[1]]$FM, list_Lc_runs[[1]]$B_R, type = 'l',
+         axes = F, ylab = '', xlab ='', lty = Lc_start,
+         col = 'blue')
+    axis(side = 4, at = pretty(range(list_Lc_runs[[1]]$B_R)), col = 'blue',
+         col.axis = 'blue')
+    mtext(side = 4, text = "B/R", line = 3, col = 'blue')
+    for(j in seq_Lc){
+      lines(list_Lc_runs[[j]]$FM, list_Lc_runs[[j]]$B_R, type = 'l',
+            ylab = "Y/R", xlab = "F", col='blue', lty = j)
+    } # only works with plotting whole graph if values are not getting bigger right? because otherwise graphs is not insed plotting area
+  }
 } ## problem of two cases: Tc and Co are given or Lc and Co. case dependent or different functions?
-
-
-
-
---
+++ R/Predict_ThompsonBell.R
@@ -0,0 +1,131 @@
+#' @title Thompson and Bell prediction model
+#
+#' @description  This is a function to calculate the total mortality (Z) from length composition data via the length converted catch curve or from age at length data with catch curve.
+#'
+#' @param classes Midpoints of the length class as vector (length frequency data) or ages as vector (age composition data).
+#' @param catch Catch as vector, or a matrix with catches of subsequent years if the catch curve with constat time intervals should be applied.
+#' @param datatype Type of data which is used for analysis, either 'length' or 'age', for length frequency or age composition data, respectively
+#' @param Linf Infinite length for investigated species in cm [cm].
+#' @param K Growth coefficent for investigated species per year [1/year].
+#' @param t0 Theoretical time zero, at which individuals of this species hatch (default: 0).
+#' @param Winf Infinite weight in grams!
+#'
+#' @param FM reference F-at-age-array
+#'
+#' @examples
+#'
+#' @details For variable parameter system vectors are reuqired for constant parameter systems matrices or data.frames have to be inserted. or vectors The length converted linearised catch curve is used to calculate the total mortality (Z). This function includes a so called locator function, which asks you to choose points from a graph manually. Based on these points the regression line is calculated.
+#'
+#' @references
+#' example 1 : Kuwait (Garcia and van zalinge 1982)
+#'
+#' @export
+
+data("ex.Predict_ThompsonBell")
+classes = ex.Predict_ThompsonBell$age
+mean_weight = ex.Predict_ThompsonBell$meanWeight
+value = ex.Predict_ThompsonBell$valueGramm
+FM = ex.Predict_ThompsonBell$FM
+Z = ex.Predict_ThompsonBell$Z
+datatype = 'age'
+Tr = 1
+stock_size_1 = NA
+unit.time = 'month'
+
+Predict_ThompsonBell <- function(classes, mean_weight, FM, Z, value = NA, Tr,
+                                 datatype, stock_size_1 = NA){
+
+  df.TB <- cbind(classes,mean_weight,value,FM,Z)
+  df.TB <- as.data.frame(df.TB)
+  df.TB$classes <- as.character(df.TB$classes)
+
+  # create column without plus group (sign) if present
+  classes.num <- do.call(rbind,strsplit(df.TB$classes, split="\\+"))
+  df.TB$classes.num <- as.numeric(classes.num[,1])
+
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  #                        Age data                          #
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  #if(datatype == 'age'){
+
+
+
+
+    # delta t
+    df.TB$dt <- NA
+    for(x0 in 1:(length(df.TB$dt)-1)){
+      df.TB$dt[x0] <- df.TB$classes.num[x0+1] - df.TB$classes.num[x0]
+    }
+    if(unit.time == 'month'){
+      df.TB$dt <- df.TB$dt * 1/12
+    }
+
+    #population per age group
+    df.TB$population <- NA
+    if(!is.na(stock_size_1)){
+      df.TB$population[1] <- stock_size_1
+    }else df.TB$population[1] <- 1000  #if not provided resulta re just negative
+
+    for(x1 in 2:length(df.TB$population)){
+      df.TB$population[x1] <- df.TB$population[x1-1] * exp(-df.TB$Z[x1]*df.TB$dt[x1])
+    }
+
+    #number of deaths per time step month or year
+    df.TB$num.deaths <- NA
+    for(x2 in 1:length(df.TB$num.deaths)){
+      df.TB$num.deaths[x2] <- df.TB$population[x2] - df.TB$population[x2+1]
+    }
+
+    #catch in numbers
+    df.TB$num.caught <- NA
+    for(x3 in 1:length(df.TB$num.caught)){
+      df.TB$num.caught[x3] <- df.TB$num.deaths[x3] * (df.TB$FM[x3] / df.TB$Z[x3])
+    }
+
+    #yield in kg or g
+    df.TB$yield <- NA
+    for(x4 in 1:length(df.TB$yield)){
+      df.TB$yield[x4] <- df.TB$num.caught[x4] * df.TB$mean_weight[x4]
+    }
+
+    #mean biomass in kg or g
+    df.TB$biomass <- NA
+    for(x5 in 1:length(df.TB$biomass)){
+      df.TB$biomass[x5] <- df.TB$yield[x5] / (df.TB$FM[x5] * df.TB$dt[x5])
+    }
+
+    #value expressed in money units
+    df.TB$value_yield <- NA
+    for(x6 in 1:length(df.TB$value_yield)){
+      df.TB$value_yield[x6] <- df.TB$yield[x6] * df.TB$value[x6]
+    }
+
+    #total yield, value and average biomass
+    total.yield <- sum(df.TB$yield,na.rm=TRUE)
+    total.value <- sum(df.TB$value_yield,na.rm=TRUE)
+    mean.biomass <- sum((df.TB$biomass * df.TB$dt),na.rm=TRUE) /
+      sum(df.TB$dt,na.rm=TRUE)   ### more complicated biomass concept if dt not constant, see Chapter 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  #                       Length data                        #
+  #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
+  if(datatype == 'length'){
+    }
+} ## problem of two cases: Tc and Co are given or Lc and Co. case dependent or different functions?
+
