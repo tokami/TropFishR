3772eba0d38a7bc1a5ef4a850017cd6a9d401f67
tokami <tobi_mildenberger@yahoo.de>
2015-12-01 11:52:06 +0100

    Z_CPUE updated

--- R/Z_CPUE.R
+++ R/Z_CPUE.R
@@ -1,51 +1,102 @@
-#' @title Methods to calculate Z from CPUE data
+#' @title Model to calculate Z from CPUE data
 #'
-#' @description Method to estimate the total mortality (Z) from CPUE data.
-#'    First sentence. second sentence.
+#' @description Model to estimate the instantaneous total mortality rate (Z) from
+#'    CPUE data according to standard, Heincke's, or Robson & Chapman's method.
 #'
-#' @param cohort bla bla
-#' @param age bla bla
-#' @param CPUE bla bla
-#' @param method One of 3 methods to caluclate Z: "standard", "Heincke"
+#' @param param A list consisting of following parameters:
+#'    \code{$cohort} a vector with with a cohort label,
+#'    \code{$age} a vector with ages,
+#'    \code{$CPUE} a vector with Catch per unit of effort (CPUE) values;
+#' @param method A character string indicating which assessment method should be used:
+#'    \code{"standard"}, \code{"Heincke"}, or \code{"RobsonChapman"}.
+#' @param omit_age1 A logical parameter indicating if the first age group should omitted.
 #'
 #' @examples
-#' data("ex.Z_CPUE")
-#' with(ex.Z_CPUE,Z_CPUE(cohort, age.t1, CPUE, method = "standard"))
+#' # load data
+#' data("synCPUE")
 #'
-#' @details Z from CPUE data
+#' # run model
+#' Z_CPUE(synCPUE, method = "standard")
 #'
-#' @return A matrix with total moratlity rates with column names ... and row names ... .
+#' # alternative method:
+#' Z_CPUE(synCPUE, method = "RobsonChapman", omit_age1  = TRUE)
+#'
+#' @details In Heincke's and RobsonChapman's method age groups older than 4 are lumped,
+#'   because age groups older than 3 or 4 are said to be hard to seperate (Ricker, 1975).
+#'   Sparre and Venema (1998) recommend to omit the first age group in case it is not fully
+#'   exploited by the fishery.
+#'
+#' @return A list with input parameters and a Z value or matrix.
 #'
 #' @references
-#' Jones ???  Sparre?
+#' Sparre, P., Venema, S.C., 1998. Introduction to tropical fish stock assessment.
+#' Part 1. Manual. FAO Fisheries Technical Paper, (306.1, Rev. 2). 407 p.
+#'
+#' Sparre, P., Venema, S.C., 1999. Introduction to tropical fish stock assessment.
+#' Part 2. Excercises. FAO Fisheries Technical Paper, (306.2, Rev. 2). 94 p.
+#'
+#' Ricker, W.E., 1975. Computation and interpretation of biological statistics of fish
+#' populations. \emph{Bull.Fish.Res.Board Can.}, (191):382 p.
 #'
 #' @export
 
-Z_CPUE <- function(cohort, age, CPUE, method){
+Z_CPUE <- function(param, method = "standard", omit_age1 = FALSE){
 
-  if(method == "standard"){
-    df.HZ <- data.frame(cohort = cohort[1:(length(cohort)-1)])
+  res <- param
+  cohort <- res$cohort
+  classes <- as.character(res$age)
+  CPUE <- res$CPUE
 
-    result_Z <- list()
-    for(i in 2:length(age)){
-      Zi <- rep(NA,(length(age)-1))
-      for(k in 1:(i-1)){
-        Zi[k] <- round((1 / (age[i] - age[k])) *
-                         (log(CPUE[k] /CPUE[i])),digits = 2)
-      }
-      result_Z[[i-1]] <- Zi
-    }
+  # create column without plus group (sign) if present
+  classes.num <- do.call(rbind,strsplit(classes, split="\\+"))
+  classes.num <- as.numeric(classes.num[,1])
 
-    for(i in 1:length(result_Z)){
-      df.HZ[[paste(cohort[i+1])]] <- result_Z[[i]]
-    }
-    print(df.HZ)
-  } else if(method == "Heincke"){
-    CPUE[4] <- sum(CPUE[4:length(CPUE)])
-    CPUE.H.n <- CPUE[2:length(CPUE)]
-    CPUE.H.d <- CPUE[1:length(CPUE)]
-    Z.H = - log( (sum(CPUE.H.n)) /
-                 (sum(CPUE.H.d)))
-    print(paste("Total mortality according to Heincke's method: ",Z.H))
-  }
+  switch(method,
+
+         "standard" ={
+           df.HZ <- data.frame(cohort = cohort[1:(length(cohort)-1)])
+           result_Z <- list()
+           for(i in 2:length(classes.num)){
+             Zi <- rep(NA,(length(classes.num)-1))
+             for(k in 1:(i-1)){
+               Zi[k] <- round((1 / (classes.num[i] - classes.num[k])) *
+                                (log(CPUE[k] /CPUE[i])),digits = 2)
+             }
+             result_Z[[i-1]] <- Zi
+           }
+           for(i in 1:length(result_Z)){
+             df.HZ[[paste(cohort[i+1])]] <- result_Z[[i]]
+           }
+           ret <- c(res,list(
+             Z_mat = df.HZ
+           ))
+           return(ret)
+         },
+
+         "Heincke" ={
+           CPUE[4] <- sum(CPUE[4:length(CPUE)])
+           if(omit_age1) CPUE <- CPUE[-1]
+           CPUE.H.n <- CPUE[2:length(CPUE)]
+           CPUE.H.d <- CPUE[1:length(CPUE)]
+           Z.H = - log( (sum(CPUE.H.n)) /
+                          (sum(CPUE.H.d)))
+           ret <- c(res,list(
+             Z = Z.H
+           ))
+           return(ret)
+         },
+         "RobsonChapman" ={
+           CPUE[4] <- sum(CPUE[4:length(CPUE)])
+           if(omit_age1) CPUE <- CPUE[-1]
+           sum_CPUE.H.n <- sum(CPUE[2:length(CPUE)])
+           sum_CPUE.H.d <- sum(CPUE[1:length(CPUE)])
+
+           Z.H = - log((sum_CPUE.H.n) /
+                          (sum_CPUE.H.d + sum_CPUE.H.n - 1))
+
+           ret <- c(res,list(
+             Z = Z.H
+           ))
+           return(ret)
+         })
 }
