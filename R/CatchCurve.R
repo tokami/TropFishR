f9ba474b38464fc6c7bb42a9304da0ea869fa041
tokami <tobi_mildenberger@yahoo.de>
2015-12-01 14:56:47 +0100

    CatchCurve updated

--- R/CatchCurve.R
+++ R/CatchCurve.R
@@ -4,40 +4,41 @@
 #'   composition data via the length converted catch curve or from age at length data
 #'   with catch curve.
 #'
-#' @param classes Midpoints of the length class as vector (length frequency data) or ages as vector (age composition data).
-#' @param catch The catch as vector, or a matrix with catches of subsequent years if the catch curve with constat time intervals should be applied.
-#' @param datatype Type of data which is used for analysis, either 'length' or 'age', for length frequency or age composition data, respectively
-#' @param Linf Infinite length for investigated species in cm [cm
-#' @param K Growth coefficent for investigated species per year [1/year].
-#' @param t0 Theoretical time zero, at which individuals of this species hatch (default: 0).
+#' @param param A list consisting of following parameters:
+#'   \code{$age} or \code{$midLengths} midpoints of the length class as vector (length frequency
+#'   data) or ages as vector (age composition data),
+#'   \code{$Linf} Infinite length for investigated species in cm [cm],
+#'   \code{$K} Growth coefficent for investigated species per year [1/year],
+#'   \code{t0} Theoretical time zero, at which individuals of this species hatch,
+#'   \code{M} Natural mortality [1/year],
+#'   \code{a} length-weight relationship coefficent (W = a * L^b),
+#'   \code{b} length-weight relationship coefficent (W = a * L^b),
+#'   \code{catch} Catch as vector, or a matrix with catches of subsequent years if
+#'   the catch curve with constat time intervals should be applied;
+#' @param catch_column A number indicating the column of the catch matrix which should be
+#'   used for the anaalysis.
+#'
 #'
 #' @examples
 #' \donttest{
-#' # Variable paramter system
+#' # Variable paramter system (with catch vector)
 #' # based on length frequency data
 #' # load data
-#' data("ex.LengthCC")   #### == goatfish (list)
+#' load(goatfish)
 #'
 #' # run model
-#' output <- with(ex.LengthCC,CatchCurve(classes = ex.LengthCC[,1], catch = ex.LengthCC[,2],
-#'   Linf = 23.1, K = 0.59, datatype = 'length'))
-#'
-#' # investigate results
-#' output
+#' CatchCurve(goatfish)
 #'
 #' # based on age composition data
 #' # load data
-#' data("ex.CatchCurve")   ### == whiting
+#' data(whiting)
 #'
 #' # run model
-#' output <- with(ex.CatchCurve,CatchCurve(age, ex.CatchCurve[,2],datatype = 'age'))
+#' CatchCurve(whiting, catch_column = 1)
 #'
-#' output
+#' # Constant parameter system based on age composition data (with catch matrix)
+#' CatchCurve(whiting)
 #'
-#' # Constant parameter system based on age composition data
-#' output <- with(ex.CatchCurve,CatchCurve(age, ex.CatchCurve[,2:8],datatype = 'age'))
-#'
-#' output
 #'  }
 #'
 #' @details For variable parameter system vectors are reuqired for constant parameter
@@ -52,7 +53,13 @@
 #'
 #' @export
 
-CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
+CatchCurve <- function(param, catch_column = NA){
+
+  res <- param
+  if(is.na(catch_column)) catch <- res$catch
+  if(!is.na(catch_column)) catch <- res$catch[,catch_column]
+  if("midLengths" %in% names(res)) classes <- as.character(res$midLengths)
+  if("age" %in% names(res)) classes <- as.character(res$age)
 
   # Error message if catch and age do not have same length
   if(class(catch) == 'matrix' | class(catch) == 'data.frame'){
@@ -61,13 +68,9 @@ CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
     if(length(classes) != length(catch)) stop("Ages and catch do not have the same length!")
   }
 
-  df.CC <- cbind(classes,catch)
-  df.CC <- as.data.frame(df.CC)
-  df.CC$classes <- as.character(df.CC$classes)
-
   # create column without plus group (sign) if present
-  classes.num <- do.call(rbind,strsplit(df.CC$classes, split="\\+"))
-  df.CC$classes.num <- as.numeric(classes.num[,1])
+  classes.num <- do.call(rbind,strsplit(classes, split="\\+"))
+  classes.num <- as.numeric(classes.num[,1])
 
 
   #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
@@ -75,29 +78,34 @@ CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
   #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
   if(class(catch) == 'matrix' | class(catch) == 'data.frame'){
 
-    if(datatype == 'length') stop("The catch curve with constant time interval is not applicable to length frequency data.")
+    if("midLengths" %in% names(res) == TRUE) stop("The catch curve with constant time interval is not applicable to length frequency data.")
 
     #HHHHHHHHHHHHHHHHHHHHHHHHHHHH#
     #   Aged based Catch curve   #
     #HHHHHHHHHHHHHHHHHHHHHHHHHHHH#
-    if(datatype == 'age'){
+    if("age" %in% names(res) == TRUE){
 
       #find cohort to analyse
       real.cohort <- diag(as.matrix(catch))
-      df.CC$catch.cohort <- c(real.cohort,
-                              rep(NA,length(df.CC$classes.num) - length(real.cohort)))
+      catch.cohort <- c(real.cohort,
+                              rep(NA,length(classes.num) - length(real.cohort)))
 
       # ln( Catch )     ==    y
-      df.CC$lnC <- log(df.CC$catch.cohort)
+      lnC <- log(catch.cohort)
+
+      #for plot
+      minlnC <- ifelse(min(lnC,na.rm=TRUE) < 0, min(lnC,na.rm=TRUE),0)
+      maxlnC <- max(lnC,na.rm=TRUE) + 1
 
       #identify plot
-      plot(x = df.CC$classes.num,y = df.CC$lnC,
+      plot(x = classes.num,y = lnC, ylim = c(minlnC,maxlnC),
            xlab = "Age [yrs]", ylab = "ln(C)")
       print("Please choose the minimum and maximum point in the graph to include for the regression line!")
-      cutter <- identify(x = df.CC$classes.num, y = df.CC$lnC,
-                         labels = rownames(df.CC), n=2)
+      cutter <- identify(x = classes.num, y = lnC,
+                         labels = order(classes.num), n=2)
 
       #calculations + model
+      df.CC <- as.data.frame(cbind(classes.num,lnC))
       df.CC.cut <- df.CC[cutter[1]:cutter[2],]
       lm1 <- lm(lnC ~ classes.num, data = df.CC.cut)
       sum_lm1 <- summary(lm1)
@@ -113,30 +121,28 @@ CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
       SE_Z_lm1 <- abs(se_slope_lm1) * qt(0.975,sum_lm1$df[2])
 
       #final plot
-      plot(x = df.CC$classes.num, y = df.CC$lnC,
+      plot(x = classes.num, y = lnC, ylim = c(minlnC,maxlnC),
            xlab = "Age [yrs]", ylab = "ln(C)",
-           cex = 1.5, bty = 'n')
+           cex = 1.5)
       par(new=T)
       points(x = df.CC.cut$classes.num, y = df.CC.cut$lnC,
              pch = 19, col = 'blue', cex = 1.5)
-      segments(x0=df.CC$classes.num[cutter[1]],
-               y0=df.CC$lnC[cutter[1]],
-               x1=df.CC$classes.num[cutter[2]],
-               y1=df.CC$lnC[cutter[2]],
+      segments(x0=classes.num[cutter[1]],
+               y0=lnC[cutter[1]],
+               x1=classes.num[cutter[2]],
+               y1=lnC[cutter[2]],
                col="blue",lwd = 1.7)
       mtext(side = 3, text = paste("Z =",round(Z_lm1,2),"+/-",
                                    round(SE_Z_lm1,2)), col = 'blue')
-      plot1 = recordPlot()
-
 
       #save all in list
-      results.CC <- list()
-      results.CC[[1]] <- df.CC
-      results.CC[[2]] <- paste("Z =",round(Z_lm1,2),"+/-", round(SE_Z_lm1,2))
-      results.CC[[3]] <- plot1
-      names(results.CC) <- c("Dataframe","Total_mortality","Plot")
-
-      return(results.CC)
+      ret <- c(res,list(
+        classes.num = classes.num,
+        lnC = lnC,
+       Z =  Z_lm1,
+       se = SE_Z_lm1
+      ))
+      return(ret)
     }
   }
 
@@ -148,37 +154,48 @@ CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
     #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
     #      Length converted catch curve     #
     #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
-    if(datatype == 'length' & (is.na(Linf) | is.na(K))) stop("You need to assign values to Linf and K for the Catch curve based on length frequency data!")
-    if(datatype == 'length'){
+    if("midLengths" %in% names(res) == TRUE){
+
+      Linf <- res$Linf
+      K <- res$K
+      t0 <- ifelse("t0" %in% names(res), res$t0, 0)
+
+      if((is.null(Linf) | is.null(K))) stop("You need to assign values to Linf and K for the Catch curve based on length frequency data!")
 
       #calculate size class interval
-      df.CC$midLengths.CC <- df.CC$classes.num
-      interval.CC <- df.CC$midLengths.CC[2] - df.CC$midLengths.CC[1]
+      midLengths <- classes.num
+      interval <- midLengths[2] - midLengths[1]
 
       # t of lower length classes
-      df.CC$lowerLengths.CC <- df.CC$midLengths.CC - (interval.CC / 2)
-      df.CC$t_L1 <- (t0 - (1/K)) * log(1 - (df.CC$lowerLengths.CC / Linf))
+      lowerLengths <- midLengths - (interval / 2)
+      t_L1 <- (t0 - (1/K)) * log(1 - (lowerLengths / Linf))
 
       # delta t
-      df.CC$dt <- NA
-      for(x1 in 1:(length(df.CC$dt)-1)){
-        df.CC$dt[x1] <- df.CC$t_L1[x1+1] - df.CC$t_L1[x1]
+      dt <- rep(NA,length(midLengths))
+      for(x1 in 1:(length(dt)-1)){
+        dt[x1] <- t_L1[x1+1] - t_L1[x1]
       }
 
       # t of midlengths
-      df.CC$t_midL <- (t0 - (1/K)) * log(1 - (df.CC$midLengths.CC / Linf))
+      t_midL <- (t0 - (1/K)) * log(1 - (midLengths / Linf))
       # ln( Catch / delta t)
-      df.CC$lnC_dt <- log(df.CC$catch / df.CC$dt)
+      lnC_dt <- log(catch / dt)
 
+      lnC_dt[which(lnC_dt == -Inf)] <- NA   ### OR zero???
+
+      #for plot
+      minlnC_dt <- ifelse(min(lnC_dt,na.rm=TRUE) < 0, min(lnC_dt,na.rm=TRUE),0)
+      maxlnC_dt <- max(lnC_dt,na.rm=TRUE) + 1
 
       #identify plot
-      plot(x = df.CC$t_midL,y = df.CC$lnC_dt,
+      plot(x = t_midL,y = lnC_dt, ylim = c(minlnC_dt,maxlnC_dt),
            xlab = "Relative age [yrs]", ylab = "ln(C/dt)")
       print("Please choose the minimum and maximum point in the graph to include for the regression line!")
-      cutter <- identify(x = df.CC$t_midL, y = df.CC$lnC_dt,
-                         labels = rownames(df.CC), n=2)
+      cutter <- identify(x = t_midL, y = lnC_dt,
+                         labels = order(t_midL), n=2)
 
       #calculations + model
+      df.CC <- as.data.frame(cbind(t_midL,lnC_dt))
       df.CC.cut <- df.CC[cutter[1]:cutter[2],]
       lm1 <- lm(lnC_dt ~ t_midL, data = df.CC.cut)
       sum_lm1 <- summary(lm1)
@@ -195,58 +212,61 @@ CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
 
 
       #final plot
-      plot(x = df.CC$t_midL, y = df.CC$lnC_dt,
+      plot(x = t_midL, y = lnC_dt, ylim = c(minlnC_dt,maxlnC_dt),
            xlab = "Relative age [yrs]", ylab = "ln(C / dt)",
-           cex = 1.5, bty = 'n')
+           cex = 1.5)
       par(new=T)
       points(x = df.CC.cut$t_midL, y = df.CC.cut$lnC_dt,
              pch = 19, col = 'blue', cex = 1.5)
-      segments(x0=df.CC$t_midL[cutter[1]],
-               y0=df.CC$lnC_dt[cutter[1]],
-               x1=df.CC$t_midL[cutter[2]],
-               y1=df.CC$lnC_dt[cutter[2]],
+      segments(x0=t_midL[cutter[1]],
+               y0=lnC_dt[cutter[1]],
+               x1=t_midL[cutter[2]],
+               y1=lnC_dt[cutter[2]],
                col="blue",lwd = 1.7)
       mtext(side = 3, text = paste("Z =",round(Z_lm1,2),"+/-",
                                    round(SE_Z_lm1,2)), col = 'blue')
-      plot1 = recordPlot()
-
 
       #save all in list
-      results.CC <- list()
-      results.CC[[1]] <- df.CC
-      results.CC[[2]] <- paste("Z =",round(Z_lm1,2),"+/-", round(SE_Z_lm1,2))
-      results.CC[[3]] <- plot1
-      names(results.CC) <- c("Dataframe","Total_mortality","Plot")
-
-      return(results.CC)
+      ret <- c(res,list(
+        t_midL = t_midL,
+        lnC_dt = lnC_dt,
+        Z =  Z_lm1,
+        se = SE_Z_lm1
+      ))
+      return(ret)
     }
 
     #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
     #    Aged based Catch curve    #
     #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH#
-    if(datatype == 'age'){
+    if("age" %in% names(res) == TRUE){
 
       # delta t
-      df.CC$dt <- NA
-      for(x1 in 1:(length(df.CC$dt)-1)){
-        df.CC$dt[x1] <- df.CC$classes.num[x1+1] - df.CC$classes.num[x1]
+      dt <- rep(NA,length(classes.num))
+      for(x1 in 1:(length(dt)-1)){
+        dt[x1] <- classes.num[x1+1] - classes.num[x1]
       }
 
       # (t + dt) / 2   ==   x
-      df.CC$tplusdt_2 <- df.CC$classes.num + (df.CC$dt / 2)
+      tplusdt_2 <- classes.num + (dt / 2)
 
       # ln( Catch / delta t)     ==    y
-      df.CC$lnC_dt <- log(df.CC$catch / df.CC$dt)
+      lnC_dt <- log(catch / dt)
 
+
+      #for plot
+      minlnC_dt <- ifelse(min(lnC_dt,na.rm=TRUE) < 0, min(lnC_dt,na.rm=TRUE),0)
+      maxlnC_dt <- max(lnC_dt,na.rm=TRUE) + 1
 
       #identify plot
-      plot(x = df.CC$tplusdt_2,y = df.CC$lnC_dt,
+      plot(x = tplusdt_2,y = lnC_dt, ylim = c(minlnC_dt,maxlnC_dt),
            xlab = "Age [yrs]", ylab = "ln(C/dt)")
       print("Please choose the minimum and maximum point in the graph to include for the regression line!")
-      cutter <- identify(x = df.CC$tplusdt_2, y = df.CC$lnC_dt,
-                         labels = rownames(df.CC), n=2)
+      cutter <- identify(x = tplusdt_2, y = lnC_dt,
+                         labels = order(tplusdt_2), n=2)
 
       #calculations + model
+      df.CC <- as.data.frame(cbind(tplusdt_2,lnC_dt))
       df.CC.cut <- df.CC[cutter[1]:cutter[2],]
       lm1 <- lm(lnC_dt ~ tplusdt_2, data = df.CC.cut)
       sum_lm1 <- summary(lm1)
@@ -263,30 +283,29 @@ CatchCurve <- function(classes, catch, datatype, Linf = NA, K = NA, t0 = 0){
 
 
       #final plot
-      plot(x = df.CC$tplusdt_2, y = df.CC$lnC_dt,
+      plot(x = tplusdt_2, y = lnC_dt, ylim = c(minlnC_dt,maxlnC_dt),
            xlab = "Age [yrs]", ylab = "ln(C / dt)",
-           cex = 1.5, bty = 'n')
+           cex = 1.5)
       par(new=T)
       points(x = df.CC.cut$tplusdt_2, y = df.CC.cut$lnC_dt,
              pch = 19, col = 'blue', cex = 1.5)
-      segments(x0=df.CC$tplusdt_2[cutter[1]],
-               y0=df.CC$lnC_dt[cutter[1]],
-               x1=df.CC$tplusdt_2[cutter[2]],
-               y1=df.CC$lnC_dt[cutter[2]],
+      segments(x0=tplusdt_2[cutter[1]],
+               y0=lnC_dt[cutter[1]],
+               x1=tplusdt_2[cutter[2]],
+               y1=lnC_dt[cutter[2]],
                col="blue",lwd = 1.7)
       mtext(side = 3, text = paste("Z =",round(Z_lm1,2),"+/-",
                                    round(SE_Z_lm1,2)), col = 'blue')
-      plot1 = recordPlot()
 
 
       #save all in list
-      results.CC <- list()
-      results.CC[[1]] <- df.CC
-      results.CC[[2]] <- paste("Z =",round(Z_lm1,2),"+/-", round(SE_Z_lm1,2))
-      results.CC[[3]] <- plot1
-      names(results.CC) <- c("Dataframe","Total_mortality","Plot")
-
-      return(results.CC)
+      ret <- c(res,list(
+        tplusdt_2 = tplusdt_2,
+        lnC_dt = lnC_dt,
+        Z =  Z_lm1,
+        se = SE_Z_lm1
+      ))
+      return(ret)
     }
   }
 }
