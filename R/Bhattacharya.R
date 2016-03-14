2a42e12389ad921d4f8d2d0834e6d95d1be3798c
tokami <tobi_mildenberger@yahoo.de>
2016-03-14 17:49:35 +0100

    Bhatta: final graphic, save mean length and SD

--- R/Bhattacharya.R
+++ R/Bhattacharya.R
@@ -1,13 +1,15 @@
 #' @title Bhattacharya's method
 #'
 #' @description Find relative frequencies and frequency distribution of distinct cohorts
-#'      in the observed length frequency distribution by resolving the distribution into
+#'      in the observed length frequency distribution by resolving it into
 #'      Gaussian components.
 #'
 #' @param param a list consisting of following parameters:
-#'   \code{$midLengths} midpoints of the length class as vector,
+#'   \code{midLengths} midpoints of the length class as vector,
 #'   \code{catch} a vector with the catch per length class or a matrix with
 #'      catches per length class of subsequent years;
+#' @param n_rnorm number of observations for the function \code{\link{rnorm}}
+#'
 #'
 #' @keywords Bhattacharya length-frequency
 #'
@@ -36,11 +38,7 @@
 #'
 #' @export
 
-
-
-param <- synLFQ1
-
-Bhattacharya <- function(param){
+Bhattacharya <- function(param, n_rnorm = 1000){
 
   res <- param
   if("midLengths" %in% names(res) == TRUE){
@@ -83,17 +81,21 @@ Bhattacharya <- function(param){
 
   bhat.table.list <- vector("list", 31)
   a.b.list <- vector("list", 30)
+  l.s.list <- vector("list", 30)
+  temp.list <- vector("list", 30)
+
   colour.xy <- c('blue','darkgreen','red','yellow','purple','orange',
                  'lightgreen','skyblue','brown','darkblue','darkorange','darkred')
   colour.vec <- rep('black', length(bhat.table$L))
 
-  print(noquote("Starting on the left, please choose from black points which lie on a straight line! Do not include points which might be affected by the next distribution!"))
-  print(noquote("To stop the process press right click (and choose 'Stop' if necessary)"))
+  writeLines("Starting on the left, please choose from black points which \nlie on a straight line! Do not include points which might be \naffected by the next distribution! \nTo stop the process press right click (and choose \n'Stop' if necessary)")
+
 
   for(xy in 1:30){  #more than 30 cohorts?
 
     #STEP 2: fills third column
     bhat.table$log.N1.plus <- round(log(bhat.table$N1.plus),digits=3)
+
 
     #STEP 3: fills fourth coulmn
     if(xy > 1) bhat.table$log.N1.plus[last_choices[2]] <- 0
@@ -107,145 +109,190 @@ Bhattacharya <- function(param){
     checki <- !is.na(bhat.table$delta.log.N1.plus) &
       bhat.table$delta.log.N1.plus != 'Inf'
     bhat.table$L[checki] <- bhat.table$mean.length.classes[checki] - (interval/2)
-#     for(i in 1:length(bhat.table$log.N1.plus)){
-#       if(!is.na(bhat.table$delta.log.N1.plus[i]) &
-#          bhat.table$delta.log.N1.plus[i] != 'Inf'){
-#         min.size <- (bhat.table$mean.length.classes[i] - (interval/2))
-#         bhat.table$L[i] <- min.size
-#       }
-#     }
+
 
 
     #STEP 5: plot of fifth against fourth column
     checki2 <- !is.na(bhat.table$delta.log.N1.plus) &
       bhat.table$delta.log.N1.plus != 'Inf'
     bhat.table$L[checki2] <- (bhat.table$mean.length.classes[checki2] - (interval/2))
-#     for(i in 1:length(bhat.table$log.N1.plus)){
-#       if(!is.na(bhat.table$delta.log.N1.plus[i]) & bhat.table$delta.log.N1.plus[i] != 'Inf'){
-#         min.size <- (bhat.table$mean.length.classes[i] - (interval/2))
-#         bhat.table$L[i] <- min.size
-#       }
-#     }
-    #STEP 5: plot of fifth against fourth column
-#     dev.new()
-#     plot(bhat.table$delta.log.N1.plus ~ bhat.table$L, pch = 16)
-#     abline(h=0)
-#     p.bhat1 <- recordPlot()
+
 
     #STEP 6: select points for regression line
     if(xy == 1){bhat.table.list[[1]] <- bhat.table}
 
-    # do not include in linear regression if number in N1.plus is under 3 or 4 if(bhat.table$N1.plus < 3){}
 
-    #find new maximum for start of next cohort
-#     if(xy > 1){
-#       gradi <- NA
-#       for(nm in 3:(length(bhat.table.list[[1]]$delta.log.N1.plus)-1)){
-#         if((bhat.table.list[[1]]$delta.log.N1.plus[nm] >
-#             bhat.table.list[[1]]$delta.log.N1.plus[nm-1]) == T &
-#            (bhat.table.list[[1]]$delta.log.N1.plus[nm] >
-#             bhat.table.list[[1]]$delta.log.N1.plus[nm+1]) == T
-#         ){gradi[nm] <- T}else gradi[nm] <- F
-#       }
-#
-#       cut.max <- max.rem + min(which(gradi[(max.rem+1):length(gradi)] == T))
-#
-#       bhat.table.list[[1]]$delta.log.N1.plus[
-#         which(as.numeric(rownames(bhat.table.list[[1]])) >= cut.max)] <-
-#         bhat.table$delta.log.N1.plus[
-#           which(as.numeric(rownames(bhat.table.list[[1]])) >= cut.max)]
-#       }
+    repeat {
+
+      if("id.co1" %in% ls()){
+        if(length(id.co1$ind) == 0) break
+      }
+
+      repeat {
+        # histogramm
+        dev.new()
+        par(mfrow=c(2,1))
+        freqis <- rep(bhat.table.list[[1]]$mean.length.classes, bhat.table.list[[1]]$N1.plus)
+        if(xy == 1) hist(freqis, breaks = 50, main = '', xlab = "L", probability = TRUE)
+        if(xy > 1) hist(freqis, breaks = 50, main = '', xlab = "L",
+                        probability = TRUE, ylim = c(0,maxlim))
+        if(xy > 1){
+          for(tempRep in 1:(xy-1)){
+            lines(temp.list[[tempRep]]$xfit, temp.list[[tempRep]]$yyfit,
+                  col=colour.xy[tempRep], lwd=1)
+          }
+        }
+        # bhatta plot
+        if(xy == 1){
+          plot(bhat.table.list[[1]]$L, bhat.table.list[[1]]$delta.log.N1.plus, pch = 16,
+               col = colour.vec)
+          abline(h = 0)
+          text(bhat.table.list[[1]]$L, bhat.table.list[[1]]$delta.log.N1.plus,
+               labels = row.names(bhat.table.list[[1]]), cex= 0.7, pos=3)
+        }
+        if(xy > 1){
+          plot(bhat.table.list[[1]]$L, bhat.table.list[[1]]$delta.log.N1.plus, pch = 4,
+               col = colour.vec)
+          abline(h = 0)
+          text(bhat.table.list[[1]]$L[1:last_choices[2]],
+               bhat.table.list[[1]]$delta.log.N1.plus[1:last_choices[2]],
+               labels = row.names(bhat.table.list[[1]][(1:last_choices[2]),]),
+               cex = 0.7, pos=3)
+          abline(a = a.b.list[[xy-1]][1], b=a.b.list[[xy-1]][2], col=colour.xy[xy-1])
+          points(bhat.table$L[(last_choices[2]+1):length(bhat.table$L)],
+                 bhat.table$delta.log.N1.plus[(last_choices[2]+1):length(bhat.table$L)],
+                 col = 'black', pch = 16)
+          text(bhat.table$L[(last_choices[2]+1):length(bhat.table$L)],
+               bhat.table$delta.log.N1.plus[(last_choices[2]+1):length(bhat.table$L)],
+               labels=row.names(bhat.table.list[[1]][
+                 (last_choices[2]+1):length(bhat.table$delta.log.N1.plus),]),
+               cex = 0.7, pos = 3)
+        }
+        # identify regression line
+        if(xy > 1) rm(id.co1)
+        id.co1 <- identify(bhat.table$L, bhat.table$delta.log.N1.plus, n = 2, pos = TRUE)
+        if(length(id.co1$ind) == 0) break
+
+        colour.vec[id.co1$ind[1]:id.co1$ind[2]] <- colour.xy[xy]
+        dev.off()
+
+        #STEP 7: calculate mean length and standard deviation of regression line
+        x.co1 <- bhat.table$L[id.co1$ind[1]:id.co1$ind[2]]
+
+        # Break loop if selection does not embrace at least 3 points
+        if(length(x.co1 < 3)) writeLines("Your selection is not possible. You have to choose two \npoints which include at least one other point. At least \nthree points are required for a regression line. Please choose again!")
+        if(length(x.co1) >= 3){
+          break
+        }
+      }
+
+      if(xy == 1 & length(id.co1$ind) == 0){
+        writeLines("You did not choose any points! Please run the function again \nand choose points to include in the calculation of the \nGaussian components of the observed frequency.")
+        cancel <- TRUE
+        break
+      }
+      if(xy > 1 & length(id.co1$ind) == 0){
+        cancel <- FALSE
+        break
+      }
+
+      y.co1 <- bhat.table$delta.log.N1.plus[id.co1$ind[1]:id.co1$ind[2]]
+      m.co1 <- lm(y.co1 ~ x.co1)
+      sum.m.co1 <- summary(m.co1)
+      a.co1 <- sum.m.co1$coefficients[1]
+      b.co1 <- sum.m.co1$coefficients[2]
+      l.mean.co1 <- -a.co1/b.co1   #mean length: L(mean)(N1) = -a/b
+      s.co1 <- sqrt(-1/b.co1)      #standard deviation: s(N1) = sqrt(-1/b)
+
+      a.b.list[[xy]] <- c(a.co1,b.co1)
+      l.s.list[[xy]] <- c(l.mean.co1,s.co1)
 
 
-    par(mfrow=c(2,1))
-    if(xy == 1){
+      #STEP 8: fill sixth column
+      #set.seed ????
+      normal.dis.co1 <- rnorm(n=n_rnorm, mean = l.mean.co1, sd=s.co1)
+      max.class.ind.co1 <- which(round(max(normal.dis.co1), digits = 0) >= (
+        bhat.table$mean.length.classes - (interval/2)) &
+          round(max(normal.dis.co1),digits = 0) < (
+            bhat.table$mean.length.classes + (interval/2)))
+      max.class.co1 <- bhat.table$mean.length.classes[max.class.ind.co1]
+      for(i in 1:max.class.ind.co1){
+        delta.N <- round(a.co1 + b.co1 * bhat.table$L[i],digits=3)
+        bhat.table$delta.log.N[i] <- delta.N
+      }
+
+
+      # Draw plot for checking
+      if(xy == 1) min.class.ind.co1 <- 1
+      if(xy > 1){
+        min.class.ind.co1 <- which(round(min(normal.dis.co1), digits = 0) >= (
+          bhat.table$mean.length.classes - (interval/2)) &
+            round(min(normal.dis.co1),digits = 0) < (
+              bhat.table$mean.length.classes + (interval/2)))
+      }
+      min.class.co1 <- bhat.table$mean.length.classes[min.class.ind.co1]
+
+      xfit <- seq(min.class.co1, max.class.co1, length = 100)
+      yfit <- dnorm(xfit, mean=l.mean.co1, sd=s.co1)
+
+      freqis <- rep(bhat.table.list[[1]]$mean.length.classes,
+                    bhat.table.list[[1]]$N1.plus)
+      h <- hist(freqis, breaks = 50, plot = FALSE)
+
+
       dev.new()
-      plot(bhat.table.list[[1]]$L, bhat.table.list[[1]]$delta.log.N1.plus, pch = 16,
-           col = colour.vec)
-      abline(h = 0)
-      text(bhat.table.list[[1]]$L, bhat.table.list[[1]]$delta.log.N1.plus,
-           labels = row.names(bhat.table.list[[1]]), cex= 0.7, pos=3)
-    }
-    if(xy > 1){
-      dev.new()
+      par(mfrow=c(2,1))
+      # histogram
+      yyfit <-  yfit * ((max(yfit) - max(h$density)) / max(yfit))
+      if(xy == 1) maxlim <- ifelse(max(yyfit) > max(h$density), max(yyfit), max(h$density))
+      hist(freqis, breaks = 50, main = "", xlab = "L",
+           probability = TRUE, ylim = c(0,maxlim))
+      if(xy > 1){
+        for(tempRep in 1:(xy-1)){
+          lines(temp.list[[tempRep]]$xfit, temp.list[[tempRep]]$yyfit,
+                col=colour.xy[tempRep], lwd=1)
+        }
+      }
+      lines(xfit, yyfit, col=colour.xy[xy], lwd=1)
+      temp.list[[xy]] <- list(xfit = xfit,
+                              yyfit = yyfit)
+      # bhatta plot
       plot(bhat.table.list[[1]]$L, bhat.table.list[[1]]$delta.log.N1.plus, pch = 4,
            col = colour.vec)
       abline(h = 0)
-      text(bhat.table.list[[1]]$L[1:last_choices[2]],
-           bhat.table.list[[1]]$delta.log.N1.plus[1:last_choices[2]],
-           labels = row.names(bhat.table.list[[1]][(1:last_choices[2]),]),
+      text(bhat.table.list[[1]]$L[1:id.co1$ind[2]],
+           bhat.table.list[[1]]$delta.log.N1.plus[1:id.co1$ind[2]],
+           labels = row.names(bhat.table.list[[1]][(1:id.co1$ind[2]),]),
            cex= 0.7, pos=3)
-
-      abline(a = a.b.list[[xy-1]][1], b=a.b.list[[xy-1]][2], col=colour.xy[xy-1])
-
-      points(bhat.table$L[(last_choices[2]+1):length(bhat.table$L)],
-             bhat.table$delta.log.N1.plus[(last_choices[2]+1):length(bhat.table$L)],
+      abline(a = a.co1, b=b.co1, col=colour.xy[xy])
+      points(bhat.table$L[(id.co1$ind[2]+1):length(bhat.table$L)],
+             bhat.table$delta.log.N1.plus[(id.co1$ind[2]+1):length(bhat.table$L)],
              col = 'black', pch = 16)
-      text(bhat.table$L[(last_choices[2]+1):length(bhat.table$L)],
-           bhat.table$delta.log.N1.plus[(last_choices[2]+1):length(bhat.table$L)],
+      text(bhat.table$L[(id.co1$ind[2]+1):length(bhat.table$L)],
+           bhat.table$delta.log.N1.plus[(id.co1$ind[2]+1):length(bhat.table$L)],
            labels=row.names(bhat.table.list[[1]][
-             (last_choices[2]+1):length(bhat.table$delta.log.N1.plus),]),
+             (id.co1$ind[2]+1):length(bhat.table$delta.log.N1.plus),]),
            cex= 0.7, pos=3)
+
+
+      repSel <- readline(prompt = writeLines("Are you satisfied with your selection and want to continue? \n'yes' or 'repeat':"))
+
+      if(repSel == '') repSel <- readline(prompt = writeLines("Please type 'yes' or 'no' into the console! \nAre you satisfied with your selection and want to continue? \n'yes' or 'repeat':"))
+
+      dev.off()
+
+      if(repSel == 'yes') break
     }
-    # Second plot with the histogramms
-#     hist(bhat.table.list[[1]]$N1.plus)
-#     plot(catch.vec ~ bhat.table.list[[1]]$mean.length.classes, type  = 'h')
-
-
-    id.co1 <- identify(bhat.table$L,bhat.table$delta.log.N1.plus,
-                       n = 2, pos = TRUE)
-    if(length(id.co1$ind) == 0){
-      break
-    }
-    colour.vec[id.co1$ind[1]:id.co1$ind[2]] <- colour.xy[xy]
-    dev.off()
-
-    #STEP 7: calculate mean length and standard deviation of regression line
-    x.co1 <- bhat.table$L[id.co1$ind[1]:id.co1$ind[2]]
-    y.co1 <- bhat.table$delta.log.N1.plus[id.co1$ind[1]:id.co1$ind[2]]
-    m.co1 <- lm(y.co1 ~ x.co1)
-    sum.m.co1 <- summary(m.co1)
-    a.co1 <- sum.m.co1$coefficients[1]
-    b.co1 <- sum.m.co1$coefficients[2]
-    l.mean.co1 <- -a.co1/b.co1   #mean length: L(mean)(N1) = -a/b
-    s.co1 <- sqrt(-1/b.co1)      #standard deviation: s(N1) = sqrt(-1/b)
-
-    a.b.list[[xy]] <- c(a.co1,b.co1)
-
-    #STEP 8: fill sixth column
-    #set.seed ????
-    normal.dis.co1 <- rnorm(n=1000, mean = l.mean.co1, sd=s.co1)
-    max.class.ind.co1 <- which(round(max(normal.dis.co1), digits = 0) >= (
-      bhat.table$mean.length.classes - (interval/2)) &
-        round(max(normal.dis.co1),digits = 0) < (
-          bhat.table$mean.length.classes + (
-            interval/2)))
-    max.class.co1 <- bhat.table$mean.length.classes[max.class.ind.co1]
-    for(i in 1:max.class.ind.co1){
-      delta.N <- round(a.co1 + b.co1 * bhat.table$L[i],digits=3)
-      bhat.table$delta.log.N[i] <- delta.N
-    }
-
 
     #STEP 9: fills one value in seventh column and one in eigth column, get clean starting value
-#     one.left.max <- which(bhat.table$N1.plus ==
-#                             max(bhat.table$N1.plus[which(!is.na(
-#                               bhat.table$delta.log.N))],na.rm=T)) - 1   #OWN ASSUMPTION: one size class left of the maximum should be not influenced by the normal distribution right to this distribution
-#     one.left.max <- one.left.max[1]
-#     if(is.na(bhat.table$log.N1.plus[one.left.max])){
-#       one.left.max <- one.left.max + 1
-#     }
-#     bhat.table$log.N1[one.left.max] <- log(bhat.table$N1.plus[
-#       one.left.max])
-#     bhat.table$N1[one.left.max] <- bhat.table$N1.plus[one.left.max]
-
     # NEW ASSUMPTION: (FIND OUT!)
+    if(length(id.co1$ind) == 0) break
     choosen <- seq(id.co1$ind[1],id.co1$ind[2],interval)
     # if odd, take midpoint:
     if(length(choosen) %% 2 == 1){
       mid_choice <- id.co1$ind[1] + (id.co1$ind[2]-id.co1$ind[1])/2
     }else{
-    # if even, take one after midpoint:
+      # if even, take one after midpoint:
       mid_choice <- (id.co1$ind[1] + (id.co1$ind[2]-id.co1$ind[1])/2) + (interval/2)
     }
     bhat.table$log.N1[mid_choice] <- log(bhat.table$N1.plus[mid_choice])
@@ -302,14 +349,7 @@ Bhattacharya <- function(param){
                                paste('N',xy,sep=''),paste('N',xy+1,'.plus',sep=''))
 
     #save data
-    #bhat.table.list[[xy+1]] <- bhat.table2 #or bhat.table1?? which one more important
     bhat.table.list[[xy+1]] <- bhat.table2 #or bhat.table1?? which one more important
-
-    #necessary for stop function to work
-    max.rem <- id.co1$ind[2]
-    last_choices <- id.co1$ind
-    rm(id.co1)
-
   }
 
   # Combine results of regression analyses for output
@@ -321,22 +361,6 @@ Bhattacharya <- function(param){
     ret_pri <- cbind(bhat.table.list[[1]], bhat.results)
   }else ret_pri <- bhat.table.list[[1]]
 
-
   ret <- list(res, bhat_results = ret_pri)
-  return(ret)
+  if(cancel != TRUE) return(ret)
 }
-
-
-
-#put that into a loop for the number of cohorts one wants to analyse,
-#in the plot give colour to all points which already were analysed or used for regression,
-#save tables to a list
-#save other important information to a list, such as total number in cohort (sum of N1 or N2), and  a and b ans stuffs
-#question if satisfied with choosing
-# have a splitted graphic device with plot to choose points and histogram where choosen
-# Gaussian probability function is displayed. Ask user if "redo" choosing.
-# First thing to show shoud be a histogram, then question if fixed number of cohort, if
-# yes how many, or no, then user can choose freely
-
-
-
