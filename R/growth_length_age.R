d65a23abea1782845c1813792f8124b38ce14271
tokami <tobi_mildenberger@yahoo.de>
2016-05-06 16:10:38 +0200

    new function for growth from LAD

---
+++ R/growth_length_age.R
@@ -0,0 +1,155 @@
+#' @title Estimation from growth parameter using length-at-age data
+#'
+#' @description  This function estimates growth parameters from length-at-age data. It
+#'    allows to perform different methods: Gulland and Holt, Ford Walford plot, or
+#'    Chapman's method.
+#'
+#' @param param a list consisting of following parameters:
+#' \itemize{
+#'   \item \strong{age}: age groups,
+#'   \item \strong{length}: corresponding length groups.
+#' }
+#' @param method indicating which of following methods should be applied: "GullandHolt",
+#'    "FordWalford", "Chapman", or "BertalanffyPlot".
+#'
+#' @examples
+#' # synthetical length at age data
+#' dat <- list(age = 1:7, length = c(25.7,36,42.9,47.5,50.7,52.8,54.2))
+#' growth_length_age(dat, method = "GullandHolt")
+#'
+#' # Bertalaffy plot
+#' dat <- list(age = c(0.64,1.16,1.65,2.1,2.64,3.21),
+#'    length = c(17.3,27.9,35.3,40.2,43.3,45.5))
+#' growth_length_age(dat, method = "BertalanffyPlot", Linf = 50)
+#'
+#' @details
+#'
+#' Gulland and Holt plot assumes
+#' infinitestimal delta t (only reasonable approximation of growth parameters if delta t
+#' is small). Ford Walford plot and Chapman assume constant time intervals between ages
+#' (delta t). The Bertalanffy plot is a robust method, however it requires an estimate of Linf. As
+#' long as this estimate is reasonable the resulting estimate of K is reasonable. For
+#' a first estimate of Linf the Powell Wetherall method \link{powell_wetherall} can
+#' be used. Otherwise, the largest fish or the average of the ten largest fish can be
+#' used for a small or large sample, respectively.
+#'
+#' @references
+#' Sparre, P., Venema, S.C., 1998. Introduction to tropical fish stock assessment.
+#' Part 1. Manual. \emph{FAO Fisheries Technical Paper}, (306.1, Rev. 2). 407 p.
+#'
+#' @export
+
+growth_length_age <- function(param, method, Linf = NA){
+
+  res <- param
+  t <- res$age
+  Lt <- res$length
+
+  delta_t <- diff(t)
+
+  switch(method,
+         "GullandHolt"={
+           # growth rate
+           y <- rep(NA,length(delta_t))
+           for(i in 1:length(delta_t)){
+             y[i] <- (Lt[i+1] - Lt[i])/delta_t[i]
+           }
+
+           #mean Lt
+           x <- rep(NA,(length(Lt)-1))
+           for(i in 1:(length(Lt)-1)){
+             x[i] <- (Lt[i+1] + Lt[i])/ 2
+           }
+
+           sum_mod <- summary(lm(y ~ x))
+           a <- sum_mod$coefficients[1]
+           b <- sum_mod$coefficients[2]
+           K <- -b
+           Linf <- -a/b
+
+           plot(y ~ x, pch = 16,
+                ylim = c(0,max(y,na.rm=TRUE)*1.1),
+                xlab = "mean L(t)",
+                ylab=expression(paste(delta,"L / ",delta,"t")),
+                main = "Gulland and Holt")
+           abline(a,b)
+
+         },
+         "FordWalford"={
+           if(!all(delta_t == 1)) stop("The Ford Walford method assumes constant time intervals!")
+
+           delta_t <- delta_t[1]
+
+           y <- Lt[2:length(Lt)]
+           x <- Lt[-length(Lt)]
+
+           sum_mod <- summary(lm(y ~ x))
+           a <- sum_mod$coefficients[1]
+           b <- sum_mod$coefficients[2]
+           K <- -1/delta_t * log(b)
+           Linf <- a/(1-b)
+
+           plot(Lt_delta_t ~ Lt_short, pch = 16, ylim = c(0,max(y,na.rm=TRUE)*1.1),
+                xlim = c(0,max(x,na.rm=TRUE)*1.1),
+                xlab = "L(t)",
+                ylab=expression(paste("L(t+",delta,"t)")),
+                main = "Ford Walford plot")
+           abline(a,b)
+           abline(1,1, lty=2)
+           segments(x0 = 0, y0 = Linf, x1 = Linf, y1 = Linf, lty=2)
+           segments(x0 = Linf, y0 = 0, x1 = Linf, y1 = Linf, lty=2)
+
+         },
+         "Chapman"={
+           if(!all(delta_t == 1)) stop("The Chapman method assumes constant time intervals!")
+
+           delta_t <- delta_t[1]
+
+           Lt_short <- Lt[-length(Lt)]
+           y <- Lt[2:length(Lt)] - Lt_short
+           x <- Lt_short
+
+
+           sum_mod <- summary(lm(y ~ x))
+           a <- sum_mod$coefficients[1]
+           b <- sum_mod$coefficients[2]
+           c <- -b
+           K <- -(1/delta_t) * log(1+b)
+           Linf <- -a/b
+
+           plot(y ~ x, pch = 16, ylim = c(0,max(y,na.rm=TRUE)*1.1),
+                xlim = c(0,max(x,na.rm=TRUE)*1.1),
+                xlab = "L(t)",
+                ylab=expression(paste("L(t+",delta,"t)-L(t)")),
+                main = "Chapman")
+           abline(a,b)
+         },
+         "BertalanffyPlot"={
+
+           #Linf should be given: otherwise using optim?
+           if(is.na(Linf)) stop("For the Bertalanffy plot you have to provide an estimate for Linf!")
+           y <- -log(1 - Lt / Linf)
+           x <- t
+
+           sum_mod <- summary(lm(y ~ x))
+           a <- sum_mod$coefficients[1]
+           b <- sum_mod$coefficients[2]
+           K <- b
+           t0 <- a/-K
+
+           plot(y ~ x, pch = 16, ylim = c(0,max(y,na.rm=TRUE)*1.1),
+                xlim = c(0,max(x,na.rm=TRUE)*1.1),
+                xlab = "t",
+                ylab=expression(paste("-ln(1-L(t)/Linf)")),
+                main = "Bertalanffy plot")
+           abline(a,b)
+         })
+
+  ret <- c(res,list(delta_t = delta_t,
+                    x = x,
+                    y = y,
+                    reg_coeffs = sum_mod$coefficients[1:2],
+                    K = K,
+                    Linf = Linf))
+  return(ret)
+}
