9b706983bbcb709b9e02294f11b0d1c2d4148b21
tokami <tobi_mildenberger@yahoo.de>
2016-11-17 14:33:28 +0100

    new tanchor function updated

--- R/startingPoint2tanchor.R
+++ R/startingPoint2tanchor.R
@@ -4,8 +4,8 @@
 #'    in TropFishR. Instead \code{t_anchor} takes on the job of anchoring VBGF growth curves on
 #'    a temporal axis. This function allows to convert FiSAT's starting points to \code{t_anchor} values
 #'
-#' @param param lfq object with dates, midLengths, catch, and growth parameters 'Linf'
-#'    and 'K' from VBGF
+#' @param param list with dates, midLengths, and catch
+#' @param par list with growth parameters 'Linf' and 'K' of VBGF
 #' @param startingLength starting length as returned by FiSAT, indicating the length within
 #'    the starting sample cut by a growth curve
 #' @param startingSample starting sample as returned by FiSAT, indicating the sample which is
@@ -14,20 +14,21 @@
 #' @keywords function lfq startingPoints t_anchor
 #'
 #' @examples
-#' data(synLFQ1)
-#' synLFQ1$Linf <- 54
-#' synLFQ1$K <- 0.6
-#' lfqNEW <- startingPoint2tanchor(synLFQ1, startingLength = 20, startingSample = 4)
+#' data(synLFQ5)
+#' lfqNEW <- startingPoint2tanchor(synLFQ5, par = list(Linf = 92, K = 0.37),
+#'    startingLength = 31, startingSample = 4)
+#' lfqRest <- lfqRestructure(lfqNEW, MA = 11)
+#' plot(lfqRest,par=list(Linf=lfqRest$Linf,K=lfqRest$K,t_anchor=lfqRest$t_anchor))
 #'
-#' @return lfq object with t_anchor value
+#' @return list with input elements and estimated t_anchor value
 #'
 #' @export
 
-startingPoint2tanchor <- function(param, startingLength, startingSample){
+startingPoint2tanchor <- function(param, par, startingLength, startingSample){
 
   res <- param
-  if(("Linf" %in% names(res)) == FALSE) stop(noquote("Please add Linf to param!"))
-  if(("K" %in% names(res)) == FALSE) stop(noquote("Please add K to param!"))
+  if(("Linf" %in% names(par)) == FALSE) stop(noquote("Please add Linf to param!"))
+  if(("K" %in% names(par)) == FALSE) stop(noquote("Please add K to param!"))
   if(("dates" %in% names(res)) == FALSE) stop(noquote("Please add a dates to param!"))
 
   if(length(res$dates) < startingSample) stop(noquote(paste("Only",
@@ -37,8 +38,9 @@ startingPoint2tanchor <- function(param, startingLength, startingSample){
 
   tx <- res$dates[startingSample]
   tx <- date2yeardec(tx)
-  agex <- VBGF(L = startingLength, param = list(Linf = res$Linf, K = res$K, t0 = 0))
-  res$t_anchor <- (tx-agex) %% floor(tx-agex)
+  agex <- VBGF(L = startingLength, param = list(Linf = par$Linf, K = par$K, t0 = 0))
+  t_anchor <- (tx-agex) %% floor(tx-agex)
 
-  return(res)
+  ret <- c(res, list(Linf = par$Linf, K = par$K, t_anchor = t_anchor))
+  return(ret)
 }
