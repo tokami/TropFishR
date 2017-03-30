e30c2520b0294607b512a68dbabd259c659b65a0
tokami <tobi_mildenberger@yahoo.de>
2017-03-29 22:38:31 -0500

    allowing more flexiblity in plotting VPA

--- R/plot.VPA.R
+++ R/plot.VPA.R
@@ -5,12 +5,21 @@
 #'
 #' @param x list of the class \code{"VPA"} containing the results of the VPA model.
 #' @param display_last_class logical; should last age/length class be displayed in graph?
+#' @param xlabel Label of the x axis
+#' @param ylabel1 Label of the first y axis
+#' @param ylabel2 Label of the second y axis
 #' @param ... standard parameters of \code{\link{barplot}}
 #'
 #' @examples
 #' data(whiting)
-#' output <- VPA(whiting, terminalF = 0.5, analysis_type = "VPA")
+#' output <- VPA(whiting, terminalF = 0.5)
 #' plot(output, display_last_class = FALSE)
+#'
+#' data(hake)
+#' output <- VPA(hake, terminalE = 0.5)
+#' plot_mat <- output$plot_mat[,-c(1,2)]  # remove first two length classes
+#' class(plot_mat) <- "VPA"
+#' plot(plot_mat, xlabel = "Midlengths [cm]")
 #'
 #' @importFrom grDevices dev.new
 #' @importFrom graphics axis barplot legend lines par plot mtext
@@ -21,18 +30,35 @@
 #'
 #' @export
 
-plot.VPA <- function(x, display_last_class = TRUE, ...){
+plot.VPA <- function(x, display_last_class = TRUE,
+                     xlabel = NA,
+                     ylabel1 = "Population",
+                     ylabel2 = "Fishing mortality",
+                     ...){
   pes <- x
-  survivors <- pes$survivors
-  FM_calc <- pes$FM_calc
-  classes.num <- pes$classes.num
-  df.VPAnew <- pes$plot_mat
+  if(!is.list(pes)){pes <- list(plot_mat = x)}
+  survivors <- pes$plot_mat[1,]
+  natLoss <- pes$plot_mat[2,]
+  catch <- pes$plot_mat[3,]
+  FM_calc <- pes$plot_mat[4,]
+  classes.num <- as.numeric(colnames(pes$plot_mat))
+  df.VPAnew <- pes$plot_mat[-4,]
 
-  if("age" %in% names(pes)) xlabel <- "Age"
-  if("midLengths" %in% names(pes)) xlabel <- "Midlength [cm]"
+  if(is.na(xlabel)){
+    if("age" %in% names(pes)){
+      xlabel <- "Age"
+    }else if("midLengths" %in% names(pes)){
+      xlabel <- "Midlength [cm]"
+    }else{
+      xlabel = "NA"
+    }
+  }else{
+    xlabel = xlabel
+  }
+
 
   #save x axis positions
-  max_sur <- round(max(survivors,na.rm=TRUE),digits=0)
+  max_sur <- round(max(survivors+natLoss+catch,na.rm=TRUE),digits=0)
   dim_sur <- 10 ^ (nchar(max_sur)-1)
   max_FM <- ceiling(max(FM_calc,na.rm=TRUE))
   max_clas <- max(classes.num,na.rm=TRUE)
@@ -44,8 +70,8 @@ plot.VPA <- function(x, display_last_class = TRUE, ...){
   #dev.new()
   op <- par(mar = c(7, 5, 4, 5))
   barplot(df.VPAnew,col=c('darkgreen','darkmagenta','gold2'),
-          xlab = xlabel, ylab = "Population",xlim=c(0,ceiling(max(mids))),
-          ylim = c(0,max_sur), yaxs="i", ...)
+          xlab = xlabel, ylab = ylabel1,xlim=c(0,ceiling(max(mids))),
+          ylim = c(0,max_sur), yaxs="i")#, ...)
   legend("topright",
          legend = c(rownames(df.VPAnew),"fishing mortality"),
          col = c('darkgreen','darkmagenta','gold2','red'),xpd = TRUE,
@@ -58,7 +84,7 @@ plot.VPA <- function(x, display_last_class = TRUE, ...){
        type = "n", yaxs="i", axes = FALSE, bty = "n", xlab = "", ylab = "")
   lines(x=mids,y=FM_calc,col='red',lwd=2)
   axis(4, at = pretty(c(0,max_FM)),line = 1)
-  mtext("Fishing mortatlity", side=4, line=3.5)
+  mtext(ylabel2, side=4, line=3.5)
   par(op)
 }
 
