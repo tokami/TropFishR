49708dbc2ed4cd443bf7785f3d36e0e8a1549966
tokami <tobi_mildenberger@yahoo.de>
2018-06-04 14:53:21 +0200

    package rebuilt

--- R/jackknife.R
+++ R/jackknife.R
@@ -1,6 +1,7 @@
 #' @title Jack-knife method for confidence intervals for growth parameters
 #
-#' @description
+#' @description The jackknife routine can be used to estimate confidence intervals for
+#'   the growth parameters estimated with ELEFAN.
 #'
 #' @param elefanFit lfq list fitted with growth parameters estimated with ELEFAN_SA or ELEFAN_GA
 #' 
@@ -32,7 +33,7 @@ jackknife <- function(elefanFit){
                       midLengths = res$midLengths,
                       catch = res$catch[,-i])
       if(method == "GA"){
-          tmp <- ELEFAN_GA(loop_data, MA = res$MA, addl.sqrt = res$addl.sqrt,
+          tmp <- invisible(capture.output(ELEFAN_GA(loop_data, MA = res$MA, addl.sqrt = res$addl.sqrt,
                            seasonalised = res$seasonalised,
                            popSize = res$popSize,
                            maxiter = res$maxiter,
@@ -44,7 +45,7 @@ jackknife <- function(elefanFit){
                            seed = res$seed,
                            low_par = res$low_par,
                            up_par = res$up_par,
-                           plot.score = FALSE)
+                           plot.score = FALSE)))
       }
       if(method =="SA"){
           SA_time <- res$control$SA_time
@@ -52,7 +53,7 @@ jackknife <- function(elefanFit){
           nb.stop.improvement <- res$control$nb.stop.improvement
           SA_temp <- res$control$temperature
           verbose <- FALSE
-          tmp <- ELEFAN_SA(loop_data,
+          tmp <- invisible(capture.output(ELEFAN_SA(loop_data,
                            SA_time = SA_time, SA_temp = SA_temp,
                            maxit = maxit,
                            nb.stop.improvement = nb.stop.improvement,
@@ -63,7 +64,7 @@ jackknife <- function(elefanFit){
                            up_par = res$up_par,
                            seasonalised = res$seasonalised,
                            flagging.out = res$flagging.out,
-                           plot.score = FALSE)
+                           plot.score = FALSE)))
       }
         JK[[i]] <- unlist(tmp$par)
         rns[i] <- tmp$Rn_max
